b4a92347c0ebea8ea5d3c08b13b22c84d87e0e30
Guillaume Luchet <guillaume@geelweb.org>
2016-02-08 22:49:22 +0100

    Update sql datasource settings

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sed -ri "s/POSTGRES_USER/$POSTGRES_USER/" /mattermost/config/config.json
-sed -ri "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/" /mattermost/config/config.json
-sed -ri "s/POSTGRES_DB/$POSTGRES_DB/" /mattermost/config/config.json
+sed -ri "s/DB_USER/$DB_USER/" /mattermost/config/config.json
+sed -ri "s/DB_PASS/$DB_PASS/" /mattermost/config/config.json
+sed -ri "s/DB_NAME/$DB_NAME/" /mattermost/config/config.json
+sed -ri "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" /mattermost/config/config.json
 
 cd /mattermost/bin
 ./platform
